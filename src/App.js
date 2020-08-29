d78d255ad3c99df58edeb24f0870c0ef7fa18ab9
muhamad safrudin abdul azis <safrudin2443420@gmail.com>
2020-08-29 21:01:58 +0700

    make first home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      
+      <div class="header">
+        <img 
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e7/Instagram_logo_2016.svg/100px-Instagram_logo_2016.svg.png"  
+          class="appLogo" 
+        />
+        <h1>Reels</h1>
+      </div>
+
+      <div class="main">
+
+      </div>
     </div>
   );
 }
